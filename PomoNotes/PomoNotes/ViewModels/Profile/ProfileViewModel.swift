0cc7717dd49bc3a8f75b3aa008fbfb845b68cfdc
JakSchlager <jami.schlager@gmail.com>
2024-03-23 15:16:57 +0100

    feat: check item in list works

--- PomoNotes/PomoNotes/ViewModels/Profile/ProfileViewModel.swift
+++ PomoNotes/PomoNotes/ViewModels/Profile/ProfileViewModel.swift
@@ -6,11 +6,27 @@
 //
 
 import Foundation
+import FirebaseAuth
+import FirebaseFirestore
 
 class ProfileViewModel: ObservableObject {
     init() {}
     
     func toggleIsDone(item: TodoItemModel) {
+        var coppiedItem = item
+        coppiedItem.setDone(!item.isDone)
+        
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let db = Firestore.firestore()
+        
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(coppiedItem.id)
+            .setData(coppiedItem.asDictionary())
         
     }
 }
